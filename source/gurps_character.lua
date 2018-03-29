68b42f050feed67028ba9d47bb45857330369ced
Nathanael Farley <nasfarley88@gmail.com>
2018-03-29 08:32:56 +0100

    Added enums and _GCHARACTERS to characters lua.

--- source/gurps_character.lua
+++ source/gurps_character.lua
@@ -1,4 +1,47 @@
 -- Defines helpers for character building
+--
+-- An `attribute` object is defined in the following way:
+-- {
+-- name: any string,
+-- type: "advantage" | "disadvantage" | "skill" | "spell" | "basic_attribute" | "secondary_characteristic" | "attack",
+-- level: any positive integer,
+-- basedon: a table matching an attribute (e.g. one only containing name and type)
+-- difficulty: "Easy" | "Average" | "Hard" | "Very Hard" | "Wildcard"
+-- }
+
+_GCHARACTERS = {}
+
+-- Store the character in the global characters table
+function store_character(key, character)
+  _GCHARACTERS[key] = character
+end
+
+-- Turns an array into a table with identical key/value pairs
+function _identity_pairs_tbl(tbl)
+  rettbl = {}
+  for _,v in ipairs(tbl) do
+    rettbl[v] = v
+  end
+
+  return rettbl
+end
+
+-- Enums for things with allowed values
+enums = {}
+enums.type = _identity_pairs_tbl({"advantage" ,
+                                 "disadvantage" ,
+                                 "skill" ,
+                                 "spell" ,
+                                 "basic_attribute" ,
+                                 "secondary_characteristic" ,
+                                 "attack"})
+enums.difficulty = {
+  easy="Easy",
+  average="Average",
+  hard="Hard",
+  very_hard="Very Hard",
+  wildcard="Wildcard"
+}
 
 -- Create a function that returns attr if all key=value pairs match (tbl -> attr
 -- direction only)
@@ -44,11 +87,7 @@ function reduce(f, arr)
 end
 
 function is_valid_attribute(attr)
-  if attr.type and attr.name then
-    return attr
-  else
-    return nil
-  end
+  return attr.name and attr.type
 end
 
 function is_advantage(attr)
